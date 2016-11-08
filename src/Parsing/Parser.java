754dc7032dedd6a72c292f5f98cc1db78200d7c9
Flow.L <elbarto347@gmail.com>
2016-11-08 21:12:33 +0100

    WIP

--- src/Parsing/Parser.java
+++ src/Parsing/Parser.java
@@ -1,5 +1,6 @@
 package Parsing;
 
+import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Vector;
 
@@ -9,41 +10,48 @@ public class Parser {
 	Vector<String> predicates;
 	Vector<String> objects;
 	Vector<String> subjects;
-
-	public void parse(String request){
+	boolean isPath;
+	
+	public void parse(String request) throws Exception{
 		predicates=new Vector<String>();
 		objects=new Vector<String>();
 		subjects=new Vector<String>();
 		String[] temp=request.split("WHERE ");
+		if(temp.length!=2)
+			throw new Exception("Error WHERE clause");
 		String params =new String(temp[1]);
 		String temp1[]=params.split(" \\. ");
-		System.out.println("Temp1: "+Arrays.toString(temp1));
 		for(int i=0;i<temp1.length;i++){
 			String temp2[]=temp1[i].split(" ");
+			if(temp2.length!=3)
+				throw new Exception("Error WHERE clause members");
 			subjects.addElement(temp2[0]);
-			predicates.add(temp2[1]);
-			objects.add(temp2[2]);
+			predicates.add(temp2[1].replaceAll(" ", ""));
+			objects.add(temp2[2].replaceAll(" ", ""));
 		}
 
 	}
 	
 	public void test(){
-		boolean path=true;
+		isPath=false;
 		for(int i=0;i<objects.size()-1;i++){
 			if(objects.get(i).contains("?")){
-				if(subjects.get(i+1).compareTo(objects.get(i))!=0){
-					path=false;
+				System.out.println(subjects.get(i+1)+ " " + objects.get(i));
+				if(subjects.get(i+1).compareTo(objects.get(i))==0){
+					isPath=true;
+				}
+				else{
+					isPath=false;
+					break;
 				}
 			}
 		}
-		if(path){
-			System.out.println("ITS A PATH");
-		}
-		else{
-			System.out.println("IT S NOT A PATH");
-		}
 	}
 
+	public boolean isPath(){
+		return isPath;
+	}
+	
 	public Vector<String> getPredicates() {
 		return predicates;
 	}
@@ -56,15 +64,4 @@ public class Parser {
 		return subjects;
 	}
 
-	
-	public static void main(String[] args) {
-		Parser p=new Parser();
-		p.parse("SELECT ?x WHERE ?x takesCourse ?x1 . ?x1 #name ?x2 . ?x2 mabite something");
-		
-		System.out.println(p.getSubjects());
-		System.out.println(p.getPredicates());
-		System.out.println(p.getObjects());
-		
-		p.test();
-	}
 }
--- src/RDFRawParser.java
+++ src/RDFRawParser.java
@@ -9,18 +9,18 @@ public final class RDFRawParser {
 
 
 	/*http://swat.cse.lehigh.edu/onto/univ-bench.owl#University*/
-	public static void main(String args[]) throws InterruptedException{
+	public static void main(String args[]){
 
 		FileLog.createLog();
 		/*http://www.w3.org/1999/02/22-rdf-syntax-ns#type*/
 		Database db=new Database("./dataset",RDFFormat.RDFXML);
-		
+
 		Vector<String> predicates=new Vector<String>();
 		Vector<String> objects=new Vector<String>();
-		
-		
+
+
 		System.out.println("?x http://swat.cse.lehigh.edu/onto/univ-bench.owl#teacherOf http://www.Department1.University0.edu/Course0 . ?x http://swat.cse.lehigh.edu/onto/univ-bench.owl#teacherOf http://www.University509.edu");
-		
+
 		//Parser p=new Parser();
 		//p.parse("SELECT ?x WHERE ?x teacherOf http://www.Department1.University0.edu/Course0 . ?x #mastersDegreeFrom http://www.University509.edu");
 		//System.out.println(p.getPredicates()+" " + p.getObjects());
@@ -30,13 +30,29 @@ public final class RDFRawParser {
 		objects.add("http://www.University509.edu");
 		db.queryNStar(predicates, objects);
 		db.printResults();
-		
-		objects.remove(1);
+
+
+		Parser p=new Parser();
+		try {
+			p.parse("SELECT ?x WHERE ?x http://swat.cse.lehigh.edu/onto/univ-bench.owl#teacherOf http://www.Department1.University0.edu/Course0 . ?x http://swat.cse.lehigh.edu/onto/univ-bench.owl#mastersDegreeFrom http://www.University509.edu");
+			if(!p.isPath()){
+				db.queryNStar(p.getPredicates(), p.getObjects());
+				db.printResults();
+			}
+			else{
+				db.queryPath(predicates, objects);
+				db.printResults();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*objects.remove(1);
 		objects.add("http://www.University5..\\.edu");
 		System.out.println("?x teacherOf http://www.Department1.University0.edu/Course0 . ?x #mastersDegreeFrom http://www.University5**.edu");
 		db.nstarRegexp(predicates, objects);
 		db.printResults();
-		
+
 		predicates=new Vector<String>();
 		objects=new Vector<String>();
 		predicates.add("http://swat.cse.lehigh.edu/onto/univ-bench.owl#takesCourse");
@@ -45,10 +61,10 @@ public final class RDFRawParser {
 		System.out.println("?x takesCourse ?x1 . ?x1 #name GraduateCourse16");
 		objects.add("GraduateCourse16");
 		db.queryPath(predicates, objects);
-		db.printResults();
-		
+		db.printResults();*/
+
 		FileLog.endLog();
-		
+
 	}
 }
 
