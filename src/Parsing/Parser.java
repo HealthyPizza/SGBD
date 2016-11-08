c9bbb18f18627e364c40e01af40b12296b952822
flacombe <flacombe@w21.etu.info-ufr.univ-montp2.fr>
2016-11-08 18:02:31 +0100

    parsing

--- src/Parsing/Parser.java
+++ src/Parsing/Parser.java
@@ -8,22 +8,40 @@ public class Parser {
 
 	Vector<String> predicates;
 	Vector<String> objects;
-
+	Vector<String> subjects;
 
 	public void parse(String request){
 		predicates=new Vector<String>();
 		objects=new Vector<String>();
-		
+		subjects=new Vector<String>();
 		String[] temp=request.split("WHERE ");
 		String params =new String(temp[1]);
 		String temp1[]=params.split(" \\. ");
 		System.out.println("Temp1: "+Arrays.toString(temp1));
 		for(int i=0;i<temp1.length;i++){
 			String temp2[]=temp1[i].split(" ");
+			subjects.addElement(temp2[0]);
 			predicates.add(temp2[1]);
 			objects.add(temp2[2]);
 		}
 
+	}
+	
+	public void test(){
+		boolean path=true;
+		for(int i=0;i<objects.size()-1;i++){
+			if(objects.get(i).contains("?")){
+				if(subjects.get(i+1).compareTo(objects.get(i))!=0){
+					path=false;
+				}
+			}
+		}
+		if(path){
+			System.out.println("ITS A PATH");
+		}
+		else{
+			System.out.println("IT S NOT A PATH");
+		}
 	}
 
 	public Vector<String> getPredicates() {
@@ -33,5 +51,20 @@ public class Parser {
 
 	public Vector<String> getObjects() {
 		return objects;
+	}
+	public Vector<String> getSubjects() {
+		return subjects;
+	}
+
+	
+	public static void main(String[] args) {
+		Parser p=new Parser();
+		p.parse("SELECT ?x WHERE ?x takesCourse ?x1 . ?x1 #name ?x2 . ?x2 mabite something");
+		
+		System.out.println(p.getSubjects());
+		System.out.println(p.getPredicates());
+		System.out.println(p.getObjects());
+		
+		p.test();
 	}
 }
